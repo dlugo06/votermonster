cbb3749853bfef59ba347c0123086237a2ffe070
dlugo06 <diego.lugo.06@gmail.com>
2015-10-17 17:40:44 -0400

    made splitting by party private/fist step of simulation

--- world_class.rb
+++ world_class.rb
@@ -1,39 +1,53 @@
 class World
 # require_relative 'voter_module.rb'
 # include MainMenu
+attr_reader :republicans, :democrats
 
   def initialize(politicians_list_test)
     @voters_list = []
-    @politicians_list = [politicians_list_test]
+    @politicians_list = politicians_list_test
     @votes_list = []
     @republicans = []
     @democrats = []
   end
 
   def simulation
+    categorize_politicians
+    puts "The republicans are:"
+      @republicans.each{|i| puts i[0]}
+    puts "\nThe democrats are:"
+      @democrats.each{|i| puts i[0]}
   end
 
 
-#private
+
+private
 #this is meant to separate the list of all politicians to a list of democrats and a list of republicans
   def categorize_politicians
     @politicians_list.each do |politician|
       if politician[1] == "democrat"
         @democrats << politician
-        # p @democrats
       elsif politician[1] == "republican"
         @republicans << politician
-      # else
-      #   raise "Second item in politicians list array is formatted incorrectly: #{p politician[1]}"
+      else
+        raise "Second item in politicians list array is formatted incorrectly: #{p politician[1]}"
       end
     end
   end
 end
 
+# begin
+#   rescue => e
+#     puts "Look for potential nil value in @polititcians_list array."
+#     e.backtrace
+#   ensure
+# end
 
 
-pol = [["jimmy", "democrat"], ["Red", "republican"]]
-pol2 = [{"Jimmy LAST" => "Democrat"}, {"Red First" => "Republican"}]
+pol = [["jimmy", "democrat"], ["Red", "republican"], ["Glenn", "republican"], ["Megan", "democrat"]]
+
+# pol2 = [{"Jimmy LAST" => "Democrat"}, {"Red First" => "Republican"}]
 
 world = World.new(pol)
-world.categorize_politicians
+world.simulation
+# world.democrats.each {|i| puts i[0]}
