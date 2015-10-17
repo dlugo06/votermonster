6cece86aace056007da1d3a4e8c3192576759dbb
dlugo06 <diego.lugo.06@gmail.com>
2015-10-17 16:34:47 -0400

    created the instance variables and fisrt attempt at splitting politicians into sub-lists

--- world_class.rb
+++ world_class.rb
@@ -1,5 +1,39 @@
 class World
-require_relative 'voter_module.rb'
-include 'voter_module'
+# require_relative 'voter_module.rb'
+# include MainMenu
 
+  def initialize(politicians_list_test)
+    @voters_list = []
+    @politicians_list = [politicians_list_test]
+    @votes_list = []
+    @republicans = []
+    @democrats = []
+  end
+
+  def simulation
+  end
+
+
+#private
+#this is meant to separate the list of all politicians to a list of democrats and a list of republicans
+  def categorize_politicians
+    @politicians_list.each do |politician|
+      if politician[1] == "democrat"
+        @democrats << politician
+        # p @democrats
+      elsif politician[1] == "republican"
+        @republicans << politician
+      # else
+      #   raise "Second item in politicians list array is formatted incorrectly: #{p politician[1]}"
+      end
+    end
+  end
 end
+
+
+
+pol = [["jimmy", "democrat"], ["Red", "republican"]]
+pol2 = [{"Jimmy LAST" => "Democrat"}, {"Red First" => "Republican"}]
+
+world = World.new(pol)
+world.categorize_politicians
