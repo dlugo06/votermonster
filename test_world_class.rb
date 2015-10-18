37d479f6bc4bd3e957c945ab4e2e5a60b2ebca05
dlugo06 <diego.lugo.06@gmail.com>
2015-10-18 03:06:47 -0400

    creates dummy data from faker gem

--- test_world_class.rb
+++ test_world_class.rb
@@ -1,37 +1,58 @@
-require_relative "world_class"
+class Test
+  require_relative 'world_class'
+  require 'faker'
 
-def test_simulation
-  all_politicians = [{name: "jimmy blue", party: "democrat"},
-          {name: "red guy", party: "republican"},
-          {name: "terrance", party: "republican"},
-          {name: "glenn coco", party: "democrat"}]
-  all_voters = [{name: "silly bob", affiliation: "tea party"},
-                {name: "happy johnny", affiliation: "liberal"},
-                {name: "sad sandy", affiliation: "tea party"},
-                {name: "joyous jill", affiliation: "liberal"},
-                {name: "mellow mandy", affiliation: "neutral"},
-                {name: "crazy craig", affiliation: "socialist"},
-                {name: "boring bill", affiliation: "neutral"},
-                {name: "radical randy", affiliation: "socialist"},
-                {name: "luantic larry", affiliation: "conservative"},
-                {name: "stayathome gunman", affiliation: "conservative"}]
-  world_sim = World.new(all_politicians,all_voters)
-  world_sim.simulation
-  puts "#{world_sim.votes.size} votes were counted"
-  puts "The voter breakdown is:"
-  puts "#{world_sim.tea_party_members.count} members of the Tea Party"
-  puts "#{world_sim.conservatives.count} members of the Conservative Party"
-  puts "#{world_sim.neutrals.count} members of the Neutral Party"
-  puts "#{world_sim.liberals.count} members of the Liberal Party"
-  puts "#{world_sim.socialists.count} members of the Socialist Party"
-  puts "By popular vote, the new overlord of the universe is #{world_sim.overall_winner[0][:name].capitalize}!!!"
-
-  if world_sim.democrats_on_ballot == [{name: "jimmy blue", party: "democrat"}, {name: "glenn coco", party: "democrat"}]
-    puts "The democrats instance works"
+  def initialize
+    @all_politicians
+    @all_voters
   end
-  if world_sim.republicans_on_ballot == [{name: "red guy", party: "republican"}, {name: "terrance", party: "republican"}]
-    puts "The republicans instance works"
+
+  def create_all_voters(t, c, n, l, s)
+    names = (t+l+n+s+c).times.map {Faker::Name.name}
+    all = []
+    all << Array.new(t, "tea party")
+    all << Array.new(l, "liberal")
+    all << Array.new(n, "neutral")
+    all << Array.new(s, "socialist")
+    all << Array.new(c, "conservative")
+    all.flatten!
+    keys = [:name, :affiliation]
+    values = []
+    names.each_with_index.map do |name, index|
+      values << [name, all[index]]
+    end
+    @all_voters = values.map{|r| Hash[r.zip(keys)].invert }
+  end
+
+  def create_all_politicians(reps, dems)
+    names = (reps+dems).times.map {Faker::Name.name}
+    keys = [:name, :party]
+    all = []
+    all << Array.new(reps, "republican")
+    all << Array.new(dems, "democrat")
+    all.flatten!
+    values = []
+    names.each_with_index.map do |name, index|
+      values << [name, all[index]]
+    end
+    @all_politicians = values.map{|r| Hash[r.zip(keys)].invert }
+  end
+
+  def test_simulation
+    world_sim = World.new(@all_politicians, @all_voters)
+    world_sim.simulation
+    puts "#{world_sim.votes.size} votes were counted"
+    puts "The voter breakdown is:"
+    puts "#{world_sim.tea_party_members.count} members of the Tea Party"
+    puts "#{world_sim.conservatives.count} members of the Conservative Party"
+    puts "#{world_sim.neutrals.count} members of the Neutral Party"
+    puts "#{world_sim.liberals.count} members of the Liberal Party"
+    puts "#{world_sim.socialists.count} members of the Socialist Party"
+    puts "By popular vote, the new overlord of the universe is #{world_sim.overall_winner[0][:name].capitalize} of the #{world_sim.overall_winner[0][:party].capitalize} Party!!!"
   end
 end
 
-test_simulation
+test_1 = Test.new
+test_1.create_all_politicians(15, 6)
+test_1.create_all_voters(3405, 252, 5424, 126, 3405)
+test_1.test_simulation
